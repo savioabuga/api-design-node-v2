6f95402a43e7da577e3354b4ee4e120b2f18dc2e
Savio Abuga <savioabuga@gmail.com>
2018-03-06 15:15:32 +0300

    working on routes

--- src/server.js
+++ src/server.js
@@ -1,16 +1,22 @@
 import express from 'express'
 import setupMiddware from './middleware'
-import { restRouter } from './api'
-import { connect } from './db'
-import { signin, protect } from './api/modules/auth'
+import {restRouter} from './api'
+import {connect} from './db'
+import {signin, protect} from './api/modules/auth'
 // Declare an app from express
 const app = express()
 
 setupMiddware(app)
 connect()
 // setup basic routing for index route
+const apiRouter = express.Router()
+apiRouter.get('/', (req, res) => {
+  res.json({api: true})
+})
 
 app.use('/signin', signin)
+
+app.use('/api', apiRouter)
 
 // catch all
 app.all('*', (req, res) => {
